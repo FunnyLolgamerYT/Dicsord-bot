c62bcc0e53478f6dd9786a114329aba4d7d29a95
EssentialBee <yarnodjong@outlook.com>
2023-05-12 11:04:41 +0200

    .

--- src/main.js
+++ src/main.js
@@ -31,5 +31,15 @@ client.on("messageCreate", (msg) => {
   }
 });
 
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.get("first-number").value;
+    const num2 = interaction.options.get("second-number").value;
+    interaction.reply(`The sum is ${num1 + num2}`);
+  }
+});
+
 //Client login
 client.login(process.env.TOKEN);
---
+++ src/register-commands.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
+
+const commands = [
+  {
+    name: "add",
+    description: "Adds two numbers",
+    options: [
+      {
+        name: "first-number",
+        description: "the first number",
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      },
+      {
+        name: "second-number",
+        description: "the second number",
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      },
+    ],
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Generating slash commands...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log("Slash commands were generated succesfully.");
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
+})();
